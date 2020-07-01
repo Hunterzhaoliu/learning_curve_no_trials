282e118c788a872ca6bf978e7c6844627349321b
Hunter Liu <hunterzhaoliu@gmail.com>
2020-07-01 12:14:00 -0500

    egg platform movement on ladder is working

--- client/src/components/Experiment.js
+++ client/src/components/Experiment.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
 import "./experiment.css";
 import background from "../images/background.png";
+import egg_platform from "../images/egg_platform.png";
+import egg from "../images/egg.png";
 
 class Experiment extends Component {
   constructor() {
@@ -14,19 +16,36 @@ class Experiment extends Component {
   }
 
   onChange = e => {
+    // console.log("e.target.value = ", e.target.value);
     this.setState({ [e.target.id]: e.target.value });
   };
 
   render() {
-    const { windowWidth } = this.props;
+    const { windowWidth, windowHeight } = this.props;
 
-    const eggPlatformWidth = 100;
+    const eggPlatformWidth = 150;
+    const eggPlatformHeight = 20;
+
+    const ladderHeight = 0.74 * windowHeight;
+
     document.documentElement.style.setProperty(
       "--egg-platform-width",
       String(eggPlatformWidth) + "px"
     );
+    document.documentElement.style.setProperty(
+      "--egg-platform-height",
+      String(eggPlatformHeight) + "px"
+    );
+    document.documentElement.style.setProperty(
+      "--ladder-height",
+      String(ladderHeight) + "px"
+    );
 
     const sliderLeft = (windowWidth - eggPlatformWidth) / 2;
+
+    const platformHeight =
+      (1 - this.state.eggHeight * 0.01) * ladderHeight - eggPlatformHeight / 2;
+
     return (
       <div>
         <img className="img-background" src={background} alt="" />
@@ -40,6 +59,12 @@ class Experiment extends Component {
             className="slider"
             id="eggHeight"
           />
+          <img
+            style={{ top: platformHeight }}
+            className="img-egg-platform"
+            src={egg_platform}
+            alt=""
+          />
         </div>
       </div>
     );
@@ -48,7 +73,8 @@ class Experiment extends Component {
 
 function mapStateToProps(state) {
   return {
-    windowWidth: state.initialize.windowWidth
+    windowWidth: state.initialize.windowWidth,
+    windowHeight: state.initialize.windowHeight
   };
 }
 //
